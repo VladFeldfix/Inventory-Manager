fbd39112fc8db6f4d4d04e3bda161dce7063cf5d
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2025-01-07 22:44:08 +0200

    general sync

--- Inventory-Manager.py
+++ Inventory-Manager.py
@@ -5,7 +5,7 @@ class main:
     # constructor
     def __init__(self):
         # load smart console
-        self.sc = SmartConsole("Inventory Manager", "1.0")
+        self.sc = SmartConsole("Inventory Manager", "1.1")
 
         # set-up main memu
         self.sc.add_main_menu_item("VIEW INVENTORY", self.view)
@@ -22,7 +22,7 @@ class main:
     ### MAIN MENU ###
     def view(self):
         # get inventory
-        inventory = self.sc.load_database("inventory.csv", ("INDEX", "DESCRIPTION", "BARCODE", "EXP. DATE"))
+        inventory = self.sc.load_database("inventory.csv", ("INDEX", "NAME", "DESCRIPTION", "BARCODE", "EXP. DATE"))
         
         # order by item description
         sorted_inventory = dict(sorted(inventory.items(), key=lambda item: item[1]))
@@ -35,6 +35,7 @@ class main:
         file.write('\t\t<table>\n')
         file.write('\t\t\t<tr>\n')
         file.write('\t\t\t\t<th>INDEX</th>\n')
+        file.write('\t\t\t\t<th>NAME</th>\n')
         file.write('\t\t\t\t<th>DESCRIPTION</th>\n')
         file.write('\t\t\t\t<th>BARCODE</th>\n')
         file.write('\t\t\t\t<th>EXP. DATE</th>\n')
@@ -47,11 +48,11 @@ class main:
                 for v in val:
                     red = ""
                     expired = ""
-                    if i == 2:
+                    if i == 3:
                         exp = self.sc.compare_dates(v, self.sc.today())
                         if exp < 0:
                             red = " class = 'red'"
-                            expired = " *Expired!"
+                            expired = " *Expired "+str(exp*-1)+" ago"
                     file.write('\t\t\t\t<td'+red+'>'+v+expired+'</td>\n')
                     i += 1
                 file.write('\t\t\t</tr>\n')
@@ -72,23 +73,25 @@ class main:
         abort = False
 
         # get items database
-        items = self.sc.load_database("items.csv", ("BARCODE","DESCRIPTION"))
+        items = self.sc.load_database("items.csv", ("BARCODE","NAME","DESCRIPTION"))
 
         # get barcode
         barcode = self.sc.input("Insert barcode")
 
         # get item description
         if barcode in items:
-            description = items[barcode]
-            description = description[0]
-            self.sc.print(description)
+            fullname = items[barcode]
+            name = fullname[0]
+            description = fullname[1]
+            self.sc.print(name+" "+description)
         else:
             self.sc.error("This barcode is not in the database")
             if self.sc.question("Would you like to add a new item to the databae?"):
+                name = self.sc.input("Insert item name")
                 description = self.sc.input("Insert item description")
-                items[barcode] = [description]
+                items[barcode] = [name,description]
                 self.sc.save_database("items.csv", items)
-                self.sc.good(barcode+" "+description+" Successfully added to database!")
+                self.sc.good(barcode+" "+name+" "+description+" Successfully added to database!")
             else:
                 abort = True
                 self.sc.warning("Procedure aborted!")
@@ -102,7 +105,7 @@ class main:
 
             # add to inventory
             # load inventory
-            inventory = self.sc.load_database("inventory.csv", ("INDEX", "DESCRIPTION", "BARCODE", "EXP. DATE"))
+            inventory = self.sc.load_database("inventory.csv", ("INDEX", "NAME", "DESCRIPTION", "BARCODE", "EXP. DATE"))
 
             # get last box id
             index = 0
@@ -115,16 +118,19 @@ class main:
                 if key >= index:
                     index = key + 1
             self.sc.print("Your index is:"+str(index))
-            inventory[index] = (description,barcode,exp)
+            inventory[index] = (name,description,barcode,exp)
             self.sc.save_database("inventory.csv", inventory)
-            self.sc.good(str(index)+" "+description+" "+barcode+" "+exp+" Successfully added to inventory!")
+            self.sc.good(str(index)+" "+name+" "+description+" "+barcode+" "+exp+" Successfully added to inventory!")
+            expired = self.sc.compare_dates(exp, self.sc.today())
+            if expired < 0:
+                self.sc.print("[X] NOTICE! this item expired "+str(expired*-1)+" days ago!", 'red')
 
         # restart
         self.sc.restart()
     
     def delete(self):
         # load inventory
-        inventory = self.sc.load_database("inventory.csv", ("INDEX", "DESCRIPTION", "BARCODE", "EXP. DATE"))
+        inventory = self.sc.load_database("inventory.csv", ("INDEX", "NAME", "DESCRIPTION", "BARCODE", "EXP. DATE"))
 
         # get item index
         index = self.sc.input("Insert item index to delete")
@@ -135,7 +141,7 @@ class main:
             self.sc.warning("Procedure aborted!")
         else:
             self.sc.print("Selected item:")
-            self.sc.print(index+" "+inventory[index][0]+" "+inventory[index][1]+" "+inventory[index][2])
+            self.sc.print(index+" "+inventory[index][0]+" "+inventory[index][1]+" "+inventory[index][2]+" "+inventory[index][3])
             if self.sc.question("Are you sure you want to delete this item?"):
                 del inventory[index]
                 self.sc.good("Successfully removed from inventory!")
